002a4cbff3909925237af156e4a41c6ce132448a
Kamalkant Bhayal <bhayalkamalkant@gmail.com>
2020-10-12 17:36:12 +0530

    Changed forcast.js file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude,longitude, callback) => {
             callback('Unable to find location',undefined)
         }
         else {
-            callback(undefined,body.currently)
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
