870a469d13e11c81340cedb8821f283c2c1c6592
Kamalkant Bhayal <bhayalkamalkant@gmail.com>
2020-10-12 16:18:34 +0530

    Remove unnecessary consol.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Welcome to app.js')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('.p1')
